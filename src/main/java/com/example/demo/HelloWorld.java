773adfa2d3c84f9b8579ca5b761cd7af15ba593b
AbhirajTulsyan <abhituls55@gmail.com>
2020-05-30 18:26:26 +0530

    first-build

---
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+@GetMapping("/hello")
+public String hello()
+{
+ return  "Hello Abhiraj, have a nice day";
+}
+    
+}
